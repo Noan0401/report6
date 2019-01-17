f45d07cfe6d1f9daeefd7c0994a3a8979efc1375
Karin OGawA <e185744@ie.u-ryukyu.ac.jp>
2019-01-17 09:24:29 +0900

    仕様変更 正規表現を使って判定できるよう検討

--- src/main/java/jp/ac/uryukyu/ie/e185706_e185744/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e185706_e185744/Main.java
@@ -2,8 +2,8 @@ package jp.ac.uryukyu.ie.e185706_e185744;
 
 public class Main {
     public static void main(String[] arg){
-        Input_subscription input_subscription = new Input_subscription();
-        String name = input_subscription.s_input(1);
+        Input input = new Input();
+        input.input_main();
 
     }
 }
